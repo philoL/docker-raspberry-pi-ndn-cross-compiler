197be2d224f92fa0d20d7706c79c1d0b8f657e5a
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2017-08-11 12:47:03 -0700

    Update build.sh with the new URL

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-: ${RPXC_IMAGE:=sdthirlwall/raspberry-pi-cross-compiler}
+: ${RPXC_IMAGE:=registry.gitlab.com/named-data/docker-raspberry-pi-ndn-cross-compiler}
 
 docker build -t $RPXC_IMAGE .
